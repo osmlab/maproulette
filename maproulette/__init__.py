65bc3b377567b702716601d56f6a4f4218a0e600
Serge Wroclawski <serge@wroclawski.org>
2013-03-30 08:36:59 -0400

    Removed old bogus functions from main app. Added helper module for misc helper functions

--- maproulette/__init__.py
+++ maproulette/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, session, request, send_from_directory, jsonify, \
     render_template, Response, url_for, redirect
+from helpers import make_json_response, parse_user_details
 from flask_oauth import OAuth
 from simplekv.fs import FilesystemStore
 from flaskext.kvsession import KVSessionExtension
@@ -8,14 +9,14 @@ import requests
 from random import choice
 from shapely.geometry import asShape, Point
 import geojson
-from xml.etree import ElementTree as ET
+
 import sys
 from flask.ext.mongoengine import MongoEngine
 
 try:
     import settings
 except ImportError:
-    sys.stderr("""There must be a settings.py file with a secret_key.
+    sys.stderr.write("""There must be a settings.py file with a secret_key.
     Run bin/make_secret.py
     """)
     sys.exit(2)
@@ -59,113 +60,6 @@ def get_osm_token(token=None):
     session.regenerate()
     return session.get('osm_token')
 
-# Some helper functions
-def parse_user_details(s):
-    """Takes a string XML representation of a user's details and
-    returns a dictionary of values we care about"""
-    root = et.find('./user')
-    if not root:
-        print 'aaaargh'
-        return None
-    user = {}
-    user['id'] = root.attrib['id']
-    user['username'] = root.attrib['display_name']
-    try:
-        user['lat'] = float(root.find('./home').attrib['lat'])
-        user['lon'] = float(root.find('./home').attrib['lon'])
-    except AttributeError:
-        pass
-    user['changesets'] = int(root.find('./changesets').attrib['count'])
-    return user
-
-def get_task(challenge, near = None, lock = True):
-    """Gets a task and returns the resulting JSON as a string"""
-    host = config.get(challenge, 'host')
-    port = config.get(challenge, 'port')
-    args = ""
-    if near:
-        args += "near=%(near)s"
-    if not lock:
-        args += "lock=no"
-    if args:
-        url = "http://%(host)s:%(port)s/task?%(args)" % {
-            'host': host,
-            'port': port,
-            'args': args}
-    else:
-        url = "http://%(host)s:%(port)s/task" % {
-            'host': host,
-            'port': port}
-    r = requests.get(url)
-    # Insert error checking here
-    return r.text
-
-def get_stats(challenge):
-    """Gets the status of a challenge and returns it as a view"""
-    host = config.get(challenge, 'host')
-    port = config.get(challenge, 'port')
-    url = "http://%(host)s:%(port)s/stats" % {
-        'host': host,
-        'port': port}
-    r = requests.get(url)
-    return make_json_response(r.text)
-
-def get_meta(challenge):
-    """Gets the metadata of a challenge and returns it as a view"""
-    host = config.get(challenge, 'host')
-    port = config.get(challenge, 'port')
-    url = "http://%(host)s:%(port)s/stats" % {
-        'host': host,
-        'port': port}
-    r = requests.get(url)
-    return make_json_response(r.text)
-
-def post_task(challenge, task_id, form):
-    """Handles the challenge posting proxy functionality"""
-    host = config.get(challenge, 'host')
-    port = config.get(challenge, 'port')
-    url = "http://%(host)s:%(port)s/task/%(task_id)s" % {
-        'host': host,
-        'port': port,
-        'task_id': task_id}
-    r = requests.post(url, data = form)
-    return make_json_response(r.text)
-
-def filter_task(difficulty = 'easy', point = None):
-    """Returns matching challenges based on difficulty and area"""
-    chgs = []
-    for name, challenge in challenges.items():
-        if challenge['meta']['difficulty'] == difficulty:
-            if point:
-                if challenge['bounds'].contains(point):
-                    chgs.append(name)
-            else:
-               chgs.append(name)
-    return chgs
-
-def task_distance(task_text, point):
-    """Accepts a task and a point and returns the distance between them"""
-    # First we must turn the task into an object from text
-    task = geojson.loads(task_text)
-    # Now find the selected feature
-    for feature in task['features']:
-        if feature['selected'] is True:
-            geom = asShape(feature)
-            return geom.distance(point)
-
-def closest_task(chgs, point):
-    """Returns the closest task by a list of challenges"""
-    # Transform point into coordinates
-    coords = point.coords
-    lat, lon = coords[0]
-    latlng = "%f,%f" % (lat, lon)
-    tasks = [get_task(chg, latlng) for chg in chgs]
-    sorted_tasks = sorted(tasks, key=lambda task: task_distance(task, point))
-    return sorted_tasks[0]
-
-def make_json_response(json):
-    """Takes text and returns it as a JSON response"""
-    return Response(json.encode('utf8'), 200, mimetype = 'application/json')
 
 # By default, send out the standard client
 @app.route('/')
@@ -173,16 +67,10 @@ def index():
     "Display the index.html"
     return render_template('index.html')
 
-@app.route('/challenges.html')
-def challenges_web():
-    "Returns the challenges template"
-    return render_template('challenges.html')
-
 @app.route('/api/challenges')
 def challenges_api():
     "Returns a list of challenges as json"
-    chgs = [challenges[c].get('meta') for c in challenges]
-    return jsonify({'challenges': chgs})
+    pass
 
 @app.route('/api/task')
 def task():
@@ -193,53 +81,29 @@ def task():
     near = request.args.get('near')
     if near:
         lat, lon = near.split(',')
-        point = Point(lat, lon)
     else:
         point = None
-    chgs = filter_task(difficulty, point)
-    # We need code to test for an empty list here
-    if near:
-        task = closest_task(chgs, point)
-    else:
-        # Choose a random one
-        challenge = choice(chgs)
-        task = get_task(challenge)
-    return make_json_response(task)
+    pass
     
-@app.route('/c/<challenge>/meta')
+@app.route('/api/c/<challenge>/meta')
 def challenge_meta(challenge):
     "Returns the metadata for a challenge"
-    if challenge in challenges:
-        return get_meta(challenge)
-    else:
-        return "No such challenge\n", 404
+    pass
 
-@app.route('/c/<challenge>/stats')
+@app.route('/api/c/<challenge>/stats')
 def challenge_stats(challenge):
     "Returns stat data for a challenge"
-    if challenge in challenges:
-        return get_stats(challenge)
-    else:
-        return "No such challenge\n", 404
+    pass
 
-@app.route('/c/<challenge>/task')
+@app.route('/api/c/<challenge>/task')
 def challenge_task(challenge):
     "Returns a task for specified challenge"
-    if challenge in challenges:
-        task = get_task(get_task(challenge, request.args.get('near')))
-        return make_json_response(task)
-    else:
-        return "No such challenge\n", 404
+    pass
 
-@app.route('/c/<challenge>/task/<id>', methods = ['POST'])
+@app.route('/api/c/<challenge>/task/<id>', methods = ['POST'])
 def challenge_post(challenge, task_id):
     "Accepts data for completed task"
-    if challenge in challenges:
-        dct = request.form
-        return post_task(challenge, task_id, dct)
-    else:
-        return "No such challenge\n", 404
-
+    pass
 
 @app.route('/logout')
 def logout():
@@ -280,7 +144,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", type = int, help = "the port to bind to",
-                        default = 6666)
+                        default = 8080)
     parser.add_argument("--host", help = "the host to bind to",
                         default = "localhost")
     args = parser.parse_args()
---
+++ maproulette/helpers.py
@@ -0,0 +1,24 @@
+"""Some helper functions"""
+from xml.etree import ElementTree as ET
+
+def make_json_response(json):
+    """Takes text and returns it as a JSON response"""
+    return Response(json.encode('utf8'), 200, mimetype = 'application/json')
+
+def parse_user_details(s):
+    """Takes a string XML representation of a user's details and
+    returns a dictionary of values we care about"""
+    root = ET.find('./user')
+    if not root:
+        print 'aaaargh'
+        return None
+    user = {}
+    user['id'] = root.attrib['id']
+    user['username'] = root.attrib['display_name']
+    try:
+        user['lat'] = float(root.find('./home').attrib['lat'])
+        user['lon'] = float(root.find('./home').attrib['lon'])
+    except AttributeError:
+        pass
+    user['changesets'] = int(root.find('./changesets').attrib['count'])
+    return user
