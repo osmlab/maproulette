a29776cce5487b0a152b490351820d4c11fb7f23
Tom MacWright <tom@macwright.org>
2013-05-08 17:48:17 -0400

    Import the necessary challenges

--- maproulette/helpers.py
+++ maproulette/helpers.py
@@ -1,6 +1,7 @@
 """Some helper functions"""
 from xml.etree import ElementTree as ET
 from flask import Response
+from models import Challenge, Task
 
 def make_json_response(json):
     """Takes text and returns it as a JSON response"""
@@ -27,7 +28,7 @@ def parse_user_details(s):
 
 def get_challenge_or_404(slug):
     """Return a challenge or 404"""
-    challenge = Challenges.get(slug)
+    challenge = Challenge.get(slug)
     if not challenge:
         abort(404)
     return challenge
