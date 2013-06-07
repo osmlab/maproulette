e7fa6ad54099d7d4ca851652fb05ddd9f7c27687
Serge Wroclawski <serge@wroclawski.org>
2013-06-06 21:32:51 -0700

    Adding challenge templates code

---
+++ maproulette/challengetemplates/default.py
@@ -0,0 +1,33 @@
+"""This module contains the data for a GeoError, the most common type
+of MapRoulette Challenge"""
+
+from models import Challenge
+import buttons
+
+class Default(Challenge):
+    dlg = {
+        'text': "This area is being loaded in your editor.\n\nDid you fix it?",
+        'buttons': [buttons.fixed, buttons.skipped]}
+
+    def set_task_status(task):
+        current = task.current
+        if current.status == 'skipped':
+            task.setavailable()
+        elif current.status == 'fixed':
+            task.setdone()
+        elif (current.status == 'alreadyfixed' 
+              or current.status == 'falsepositive'):
+            # We should see two of these before setting the task to done
+            l = [i for i in task.actions where i.status == "falsepositive" \
+                     or i.status == "alreadyfixed"]
+            if len(l) >= 2:
+                task.setdone()
+            else:
+                task.setavailble()
+        else:
+            # A TASK SHOULD NEVER GET HERE- if it does, it's due to
+            # some illegal action. Throw the task back on the work pile
+            task.setavailable()
+
+# Now register this class with the Challenge
+Challenge.types['Default'] = Default
