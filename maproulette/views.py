6e0b4caf5c2c6ee721aaade55c117d653849cf48
Serge Wroclawski <serge@wroclawski.org>
2013-06-25 14:42:38 -0400

    Cleaning up the views

--- maproulette/views.py
+++ maproulette/views.py
@@ -1,7 +1,8 @@
 import json
 
 from maproulette import app, models
-from flask import render_template, redirect, session, jsonify, abort
+from maproulette.models import Challenge, Task, Action
+from flask import render_template, redirect, session, jsonify, abort, request
 
 # By default, send out the standard client
 @app.route('/')
@@ -9,87 +10,77 @@ def index():
     "Display the index.html"
     return render_template('index.html')
 
-def get_challenge_or_404(slug):
-    return models.Challenge.query.filter_by(slug=slug).first_or_404()
-
 @app.route('/api/challenges')
 def challenges_api():
     "Returns a list of challenges as json"
+    difficulty = request.args.get('difficulty')
+    ### THIS USE OF NEAR IS PROBABLY BROKEN!!! ###
+    near = request.args.get('near')
+    if difficulty and near:
+        challenges =  Challenges.query.filter_by(
+            Challenges.difficulty==difficulty,
+            Challenges.near == near).all()
+    elif difficulty:
+        challenges = Challenges.query.filter_by(
+            Challenges.difficulty == difficulty).all()
+    elif near:
+        challenges = Challenges.query.filter_by(
+            Challenges.near == near).all()
+    else:
+        challenges = Challenges.query.all()
     return jsonify(challenges =
-        [i.slug for i in models.Challenge.query.all()])
+        [i.slug for i in challenges])
 
-@app.route('/api/challenges/<id>')
-def challenge_details(id):
+@app.route('/api/challenges/<challenge_id>')
+def challenge_details(challenge_id):
     "Returns details on the challenge"
-    challenge = models.Challenge.query.filter(models.Challenge.id==id).first()
-    if challenge:
-        return jsonify(challenge=challenge)
-    abort(404)
-
-@app.route('/api/challenge/<difficulty>')
-def pick_challenge(difficulty):
-    "Returns a random challenge based on the preferred difficulty"
-    # I don't know if there is really a random() method..
-    challenge = models.Challenge.query.filter(models.Challenge.difficulty==difficulty).random()
+    c = Challenge.query.filter(Challenge.id==challenge_id).first_or_404()
     return jsonify(challenge=challenge)
 
-@app.route('/api/task/<challenge>/<lon>/<lat>/<distance>')
-def task():
-    if not challenge:
-        # we will need something real here
-        return None
-    if lon and lat:
-        models.Task.query.filter(models.Task.challenge_id == challenge)
-    "Returns an appropriate task based on parameters"
-    pass
-
-@app.route('/api/challenges/<slug>/meta')
-def challenge_meta(slug):
+@app.route('/api/challenges/<challenge_id>/meta')
+def challenge_meta(challenge_id):
     "Returns the metadata for a challenge"
-    challenge = get_challenge_or_404(slug)
+    c = Challenge.query.filter_by(Challenge.id==challenge.id).first_or_404()
     return jsonify(challenge = {
-            'slug': challenge.slug,
-            'title': challenge.title,
-            'description': challenge.description,
-            'blurb': challenge.blurb,
+            'slug': c.slug,
+            'title': c.title,
+            'description': c.description,
+            'blurb': c.blurb,
             'help': challenge.help,
-            'doneDlg': json.loads(challenge.done_dialog),
-            'instruction': challenge.instruction})
+            'doneDlg': json.loads(c.done_dialog),
+            'instruction': c.instruction})
 
-@app.route('/api/challenges/<challenge>/stats')
-def challenge_stats(challenge):
+@app.route('/api/challenges/<challenge_id>/stats')
+def challenge_stats(challenge_id):
     "Returns stat data for a challenge"
-    c = models.Challenge.query.filter_by(slug=challenge).first_or_404()
-    # Grab the correct challenge type w/o the challenge itself (not necessary)
-    C = c.types[c.type](c.id) # Here is a bit of black magic
-    tasks = models.Tasks.query.filter_by(challenge_id=c.id)
+    c = Challenge.query.filter_by(Challenge.slug=challenge_id).first_or_404()
+    challenge_obj = models.types[c.type](c.id)
+    tasks = Tasks.query.filter_by(challenge_id=c.id)
     total = len(tasks)
-    available = len([t for t in tasks if C._get_task_available(t)])
+    available = len([t for t in tasks if challenge_obj._get_task_available(t)])
     return jsonify(stats={'total': 100, 'available': available})
 
 
-@app.route('/api/challenges/<slug>/task')
-def challenge_task(slug):
+### THIS FUNCTION IS NOT COMPLETE!!! ###
+@app.route('/api/challenges/<challenge_id>/tasks')
+def challenge_task(challenge_id):
     "Returns a task for specified challenge"
-    challenge = get_challenge_or_404(slug)
-    # Grab a random task (not very random right now)
-    task = challenge.tasks.first()
-    # Create a new status for this task
-    action = models.Action(task.id, "assigned")
-    models.db.session.add(action)
-    models.db.session.commit()
-    return jsonify(task = {
-            'challenge': challenge.slug,
-            'id': challenge.id,
-            'features': task.manifest,
-            'text': challenge.instruction})
+    # By default, we return a single task
+    num = request.args.get('num', 1)
+    # If we don't have a "near", we'll use a random function
+    near = request.args.get('near')
+    c = Challenge.query.filter_by(Challenge.slug=challenge_id).first_or_404()
+    # Each task we give should also be assigned
+    assign(task, osmid)
+    return jsonify(tasks = [])
 
-@app.route('/api/challenges/<challenge>/task/<id>')
+
+@app.route('/api/challenges/<challenge_id>/tasks/<task_id>')
 def get_task_by_id(challenge, task_id):
     "Gets a specific task by ID"
     pass
 
-@app.route('/api/challenges/<challenge>/task/<id>', methods = ['POST'])
+@app.route('/api/challenges/<challenge_id>/task/<task_id>', methods = ['POST'])
 def challenge_post(challenge, task_id):
     "Accepts data for completed task"
     pass
