9277876e11037203d945a61ebf83814c10172f71
Serge Wroclawski <serge@wroclawski.org>
2013-07-03 22:19:25 -0400

    First attempt at buttons

---
+++ maproulette/buttons.py
@@ -0,0 +1,13 @@
+"""MapRoulette Dialog Box Buttons"""
+
+YES = {'action': "fixed",
+       'label': "Yes"}
+
+NO = {'action': "skipped",
+      'label': "No"}
+
+ALREADYFIXED = {'action': "alreadyfixed",
+                'label': "Already Fixed"}
+
+FALSEPOSITIVE = {'action': "falsepositive",
+                 'label': "Not an error"}
