e58b131e4e1593ccd48ecdba8cea1aaa80388c7a
Martijn van Exel <m@rtijn.org>
2013-09-09 08:04:33 -0600

    improved loader

--- bin/load_sampledata.py
+++ bin/load_sampledata.py
@@ -14,46 +14,53 @@ from geoalchemy2.types import Geometry
 # Right now (13/08/08), for this to work you need to patch Flask-SQLALchemy
 # as described here: https://github.com/mitsuhiko/flask-sqlalchemy/pull/89
 
-identifier = 0
-tasks = []
-
-c = Challenge('test')
-c.title = 'Just a test challenge'
-c.active = True
-
-jsonfile = open('/home/mvexel/osm/maproulette/fixtures/sampledata.json').read()
-q = json.loads(jsonfile)
-
-for feature in q['features']:
-    identifier += 1
-    coordinates = feature['geometry']['coordinates']
-    shape = Point(coordinates[0], coordinates[1])
-    properties = feature['properties']
-    t = Task('test',identifier)
-    t.location = dumps(shape)
-    t.run = 1
-    tasks.append(t)
-    
-print tasks
-
-
-from sqlalchemy import create_engine
+import sys
+import os
+from sqlalchemy import create_engine   
 from sqlalchemy.orm import sessionmaker
 
-# an Engine, which the Session will use for connection
-# resources
-feedengine = create_engine('postgresql://osm:osm@localhost/maproulette')
 
-# create a configured "Session" class
-Session = sessionmaker(bind=feedengine)
+def load_sampledata(path):
 
-# create a Session
-session = Session()
+    identifier = 0
+    tasks = []
 
-# work with sess
-session.add(c)
-session.commit()
-for t in tasks:
-    session.add(t)
-session.commit()
+    c = Challenge('test')
+    c.title = 'Just a test challenge'
+    c.active = True
 
+    with open(path, 'rb') as filehandle:
+        q = json.load(filehandle)
+
+        for feature in q['features']:
+            identifier += 1
+            coordinates = feature['geometry']['coordinates']
+            shape = Point(coordinates[0], coordinates[1])
+            properties = feature['properties']
+            t = Task('test',identifier)
+            t.location = dumps(shape)
+            t.run = 1
+            tasks.append(t)
+    
+    print tasks
+    
+    feedengine = create_engine('postgresql://osm:osm@localhost/maproulette')
+
+    Session = sessionmaker(bind=feedengine)
+
+    session = Session()
+
+    session.add(c)
+    session.commit()
+    for t in tasks:
+        session.add(t)
+    session.commit()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit('this requires a file argument')
+    path = sys.argv[1]
+    if not os.path.isfile(path):
+        sys.exit('there is no file there')
+    print 'working with %s' % (path,)
+    load_sampledata(path)
