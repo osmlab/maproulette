c975d4fd73c631a6f83ecc67a22396dbc628abc3
Martijn van Exel <m@rtijn.org>
2013-12-14 13:05:35 -0700

    adding task list to new api

--- maproulette/api/__init__.py
+++ maproulette/api/__init__.py
@@ -2,8 +2,10 @@ from maproulette import app
 from flask.ext.restful import reqparse, fields, marshal, Api, Resource
 from flask.ext.restful.fields import get_value, Raw
 from flask import session
-from maproulette.helpers import GeoPoint, get_challenge_or_404
+from maproulette.helpers import GeoPoint, get_challenge_or_404, \
+    get_random_task
 from maproulette.models import Challenge, Task, Action, db
+import geojson 
 
 class GeoJsonField(Raw):
     """A GeoJson Representation of an Shapely object"""
@@ -13,6 +15,7 @@ class GeoJsonField(Raw):
         if value is None:
             return self.default
         else:
+            app.logger.debug(value)
             value = geojson.loads(value)            
         return self.format(value)
         
@@ -119,6 +122,64 @@ class ApiChallengeStats(Resource):
                 
         return {'total': total, 'available': available}
 
+class ApiChallengeTaskList(Resource):
+    def get(self, slug):
+        "Returns a task for specified challenge"
+        challenge = get_challenge_or_404(slug, True)
+        parser = reqparse.RequestParser()
+        parser.add_argument('num', type=int, default=1,
+                            help='Number of return results cannot be parsed')
+        parser.add_argument('near', type=GeoPoint,
+                            help='Near argument could not be parsed')
+        parser.add_argument('assign', type=int, default=1,
+                            help='Assign could not be parsed')
+        args = parser.parse_args()
+        osmid = session.get('osm_id')
+        # By default, we return a single task, but no more than 10
+        num = min(args['num'], 10)
+        assign = args['assign']
+        near = args['near']
+        
+        app.logger.info("{user} requesting {num} tasks from {challenge} near {near} assiging: {assign}".format(user=osmid, num=num, challenge=slug, near=near, assign=assign))
+        
+        task_list = []
+        if near:
+            coordWKT = 'POINT(%s %s)' % (near.lat, near.lon)
+            task_query = Task.query.filter(Task.location.ST_Intersects(
+                    ST_Buffer(coordWKT, app.config["NEARBUFFER"]))).limit(num)
+            task_list = [task for task in task_query
+                         if challenge.task_available(task, osmid)]
+        if not near or not task_list:
+            # If no location is specified, or no tasks were found, gather
+            # random tasks
+            task_list = [get_random_task(challenge) for _ in range(num)]
+            task_list = filter(None, task_list)
+            # If no tasks are found with this method, then this challenge
+            # is complete
+        if not task_list:
+            # Is this the right error?
+            osmerror("ChallengeComplete",
+                     "Challenge {} is complete".format(slug))
+        if assign:
+            for task in task_list:
+                action = Action(task.id, "assigned", osmid)
+                task.current_state = action
+                db.session.add(action)
+                db.session.add(task)
+            db.session.commit()
+        
+        app.logger.info(
+            "{num} tasks found matching criteria".format(num=len(task_list)))
+        
+        tasks = [marshal(task, task_fields) for task in task_list]
+
+        for query in get_debug_queries():
+            app.logger.debug(query)
+
+        return tasks
+
+
 api.add_resource(ApiChallengeList, '/api/challenges/')
 api.add_resource(ApiChallengeDetail, '/api/challenge/<string:slug>')
 api.add_resource(ApiChallengeStats, '/api/challenge/<string:slug>/stats')
+api.add_resource(ApiChallengeTaskList, '/api/challenge/<slug>/tasks')
