4072a28681ab0fa8c3c69c380f337a565b247a6e
Serge Wroclawski <serge@wroclawski.org>
2014-02-09 16:21:42 -0500

    Escape quotes for great success

--- maproulette/static/js/maproulette.js
+++ maproulette/static/js/maproulette.js
@@ -575,7 +575,7 @@ var MRManager = (function () {
     };
   
     var presentWelcomeDialog = function() {
-      var OKButton = "<div class='button' onclick='location.reload();location.href='/login'>Log in</div>"
+      var OKButton = "<div class='button' onclick='location.reload();location.href=\'/login\'>Log in</div>"
       var welcomeHTML = "<h1>Welcome to MapRoulette</h1>" 
         + "<p>Log into OpenStreetMap to play MapRoulette<p>"
         + OKButton;
