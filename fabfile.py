81a3306894697cb6ba23d39e17ced09c99eb2674
Martijn van Exel <m@rtijn.org>
2014-04-04 15:55:24 -0600

    various typos and more fixes to postgres setup

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,8 @@
 from fabric.api import run
 from fabric.colors import red
-from fabric.contrib.files import exists, cd, upload_template, append
+from fabric.contrib.files import exists, cd, upload_template, sed
 from fabric.contrib.project import rsync_project
-from fabric.operations import put, sudo
+from fabric.operations import sudo
 
 pg_hba_fname = "/etc/postgresql/9.3/main/pg_hba.conf"
 
@@ -55,7 +55,7 @@ def create_deploy_directories(instance):
 def create_virtualenv(instance):
     dirname = "/srv/www/%s" % instance
     cmd = "virtualenv %s/virtualenv" % (dirname)
-    run('su -s /bin/bash -c "%s" www-data' % cmd)
+    sudo('su -s /bin/bash -c "%s" www-data' % cmd)
 
 
 def checkout_repo(instance, branch=None):
@@ -67,22 +67,22 @@ def checkout_repo(instance, branch=None):
         cmd = "git clone https://github.com/osmlab/maproulette.git %s" %\
             (dirname)
     with cd("/srv/www"):
-        run('su -s /bin/bash -c "%s" www-data' % cmd)
+        sudo('su -s /bin/bash -c "%s" www-data' % cmd)
 
 
 def install_python_dependencies(instance):
     dirname = "/srv/www/%s" % instance
-    cmd = 'source %s/virtualenv/bin/activate && pip install'
-    ' -r %s/htdocs/maproulette/requirements.txt' % (
-        dirname, dirname)
-    run('su -s /bin/bash -c "%s" www-data' % cmd)
+    cmd = 'source %s/virtualenv/bin/activate && pip install -r %s/htdocs/maproulette/requirements.txt' % (dirname, dirname)
+    sudo('su -s /bin/bash -c "%s" www-data' % cmd)
 
 
 def setup_uwsgi_file(instance):
     sites_available_file = "/etc/uwsgi/apps-available/%s.ini" % instance
     sites_enabled_file = "/etc/uwsgi/apps-enabled/%s.ini" % instance
     upload_template("uwsgi", sites_available_file,
-                    use_jinja=True, template_dir="fabric_templates",
+                    use_sudo=True,
+                    use_jinja=True,
+                    template_dir="fabric_templates",
                     context={"instance": instance})
     sudo("ln -s %s %s" % (sites_available_file, sites_enabled_file))
 
@@ -91,7 +91,9 @@ def setup_nginx_file(instance):
     sites_available_file = "/etc/nginx/sites-available/%s" % instance
     sites_enabled_file = "/etc/nginx/sites-enabled/%s" % instance
     upload_template("nginx", sites_available_file,
-                    use_jinja=True, template_dir="fabric_templates",
+                    use_jinja=True,
+                    use_sudo=True,
+                    template_dir="fabric_templates",
                     context={"instance": instance})
     sudo("ln -s %s %s" % (sites_available_file, sites_enabled_file))
 
@@ -99,10 +101,17 @@ def setup_nginx_file(instance):
 def setup_config_file(instance, setting):
     basedir = "/srv/www/%s" % instance
     target = basedir + "/config.py"
-    configs = {
-        'prod': 'config.py.production',
-        'sergetest': 'config.py.sergetest'}
-    put('fabric_files/' + configs[setting], target)
+    upload_template("config",
+                    target,
+                    use_jinja=True,
+                    use_sudo=True,
+                    template_dir="fabric_templates",
+                    context={
+                        "instance": instance,
+                        "setting": setting,
+                        #"consumer_key": '',
+                        #"consumer_secret": ''
+                    })
     sudo('chown www-data:www-data %s' % target)
     restart_uwsgi()
 
@@ -117,16 +126,17 @@ def rsync(instance, reload_pip=False):
 
 def git_pull(instance):
     cmd = "cd /srv/www/%s/htdocs/maproulette && git pull" % instance
-    run('su -s /bin/bash -c "%s" www-data' % cmd)
+    sudo('su -s /bin/bash -c "%s" www-data' % cmd)
 
 
 def setup_postgres_permissions():
     if not exists(pg_hba_fname):
         print(red(pg_hba_fname + "is not present on the filesystem"))
         exit()
-    append(pg_hba_fname,
-           "host\tall\tall\t127.0.0.1/32\ttrust",
-           use_sudo="yes")
+    sed(pg_hba_fname,
+        "local\s+all\s+all\s+peer",
+        "local\tall\t\tall\t\t\t\t\ttrust",
+        use_sudo="yes")
     restart_postgres()
 
 
@@ -139,30 +149,33 @@ def install_postgis():
     run("wget --quiet -O - http://apt.postgresql.org/pub/"
         "repos/apt/ACCC4CF8.asc | sudo apt-key add -")
     update_packages()
-    sudo("sudo apt-get -q install Postgresql-9.3-postgis postgresql-contrib")
+    postgres_packages = ["Postgresql-9.3-postgis",
+                         "postgresql-contrib",
+                         "postgresql-server-dev-9.3"]
+    sudo("sudo apt-get -q install %s" % (' '.join(postgres_packages)))
 
 
 def create_db_user():
-    cmd = "createuser -s -P osm"
+    cmd = "createuser -s -w osm"
     sudo('su -s /bin/bash -c "%s" postgres' % cmd)
 
 
 def create_databases():
     cmd = "createdb -O osm maproulette"
-    run('su -s /bin/bash -c "%s" postgres' % cmd)
+    sudo('su -s /bin/bash -c "%s" postgres' % cmd)
     cmd = "createdb -O osm maproulette_test"
-    run('su -s /bin/bash -c "%s" postgres' % cmd)
+    sudo('su -s /bin/bash -c "%s" postgres' % cmd)
     cmd = "createdb -O osm maproulette_dev"
-    run('su -s /bin/bash -c "%s" postgres' % cmd)
-    cmd = "psql -h localhost -U osm -d maproulette"\
+    sudo('su -s /bin/bash -c "%s" postgres' % cmd)
+    cmd = "psql -U osm -d maproulette"\
         " -c 'CREATE EXTENSION postgis'"
-    run('su -s /bin/bash -c "%s" postgres' % cmd)
-    cmd = "psql -h localhost -U osm -d maproulette_test"\
+    sudo('su -s /bin/bash -c "%s" postgres' % cmd)
+    cmd = "psql -U osm -d maproulette_test"\
         " -c 'CREATE EXTENSION postgis'"
-    run('su -s /bin/bash -c "%s" postgres' % cmd)
-    cmd = "psql -h localhost -U osm -d maproulette_dev"\
+    sudo('su -s /bin/bash -c "%s" postgres' % cmd)
+    cmd = "psql -U osm -d maproulette_dev"\
         " -c 'CREATE EXTENSION postgis'"
-    run('su -s /bin/bash -c "%s" postgres' % cmd)
+    sudo('su -s /bin/bash -c "%s" postgres' % cmd)
 
 
 def setup_system():
