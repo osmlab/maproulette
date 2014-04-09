54d11e0063a69c495704cdcce0a268a3daecb5a2
Martijn van Exel <m@rtijn.org>
2014-04-09 13:25:36 -0600

    improving unassign script

--- bin/unassign.py
+++ bin/unassign.py
@@ -7,11 +7,11 @@
 
 from maproulette.models import db, Task, Action
 from sqlalchemy.sql.functions import max
-import datetime
+from datetime import datetime, timedelta
 import pytz
 
 current_time = datetime.now(pytz.utc)
-stale_threshold = current_time - datetime.timedelta(hours=1)
+stale_threshold = current_time - timedelta(hours=1)
 
 
 def get_stale_assigned_tasks():
@@ -20,13 +20,17 @@ def get_stale_assigned_tasks():
     # select t.id from tasks t, actions a where
     # a.task_id = t.id and t.currentaction = 'assigned'
     # group by t.id having now() - max(a.timestamp) < interval '1 day';
-    return db.session.query(Task).filter_by(
-        currentaction='assigned').join(Task.actions).group_by(
-        Task.id).having(max(Action.timestamp) > stale_threshold).all()
+    return db.session.query(Task).filter(
+        Task.currentaction.in_(['assigned', 'editing'])).join(
+        Task.actions).group_by(
+        Task.id).having(max(Action.timestamp) < stale_threshold).all()
 
 if __name__ == "__main__":
+    counter = 0
     for task in get_stale_assigned_tasks():
         task.append_action(Action("available"))
         db.session.add(task)
         print "setting task %s to available" % (task.identifier)
+        counter += 1
     db.session.commit()
+    print 'done. %i tasks made available' % counter
