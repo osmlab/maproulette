271d900fd5382fa02e793255215961b4160e4bb0
kristenkam <kam.kristen@gmail.com>
2014-04-27 21:19:24 -0700

    Bulk API Loader
    Augmented CristianCantoro's process.py script that uploads tasks from
    geojson files or databases with osmosis schema. See bulk_process.py. New
    script is more OOP and a easier to maintain outside of bulk_process
    script.
    
    I was unable to test the uploadTasks method of the mapRouletteChallenge
    task. I received a 405 status response from the dev server. Would like
    assistance with debugging.
    
    I also created this 'test unit' python file of statements I used to test
    classes & methods.

--- examples/classmaprouletteloader.py
+++ examples/classmaprouletteloader.py
@@ -1,113 +1,76 @@
 import hashlib
-import psycopg2
-from psycopg2.extras import register_hstore, DictCursor
 import json
 import requests
-
 
 ##MAPROULETTE CHALLENGE AND TASK LOADER CLASSES AND METHODS
 class mapRouletteChallenge(object):
     def __init__(self,server,challengeSlug,challengeTitle=None,challengeInstruction=None):
-        self.base = server + 'api'
+        self.base = server + 'api/'
         self.getchallenge_endpoint = self.base + 'challenge/{slug}'
         self.api_createchallenge_endpoint = self.base + 'admin/challenge/{slug}'
         self.api_addtask_endpoint = self.base + 'admin/challenge/{slug}/task/{id}'
+        self.api_addbulktask_endpoint = self.base + 'admin/challenge/{slug}/tasks'
         self.slug = challengeSlug
         self.title = challengeTitle
         self.instruction = challengeInstruction        
         self.payload = []
         self.challengeExists = False
+        self.serverExists = False
 
-        #checks if server is running instance of maproulette
+        #checks if server is running instance of maproulette api
         try:
-            r = requests.get(base + 'ping')
+            r = requests.get(self.base + 'ping')
             if r.status_code == 200:
-                print 'MapRoulette API Service Exists!'
+                print 'MapRoulette API Service Exists!\n'
+                self.serverExists = True
+                return None
+            else:
+                print 'MapRoulette API Service Doesn\'t seem to exist!\n'                           
         except requests.exceptions.ConnectionError:
-            print 'MapRoulette API Service Doesn\'t seem to exist!\nEnding challenge creation.'
-            return False
+            print 'MapRoulette API Service Doesn\'t seem to exist!\n'
 
-        #Determines if Challenge Exists or not
+        print 'Ending challenge creation.\n'                   
+
+    def initChallenge(self):
+        #Determines if an active challenge Exists or not
         response = requests.get(self.getchallenge_endpoint.format(slug=self.slug))
-        if response.status_code == 200:
+        if (response.status_code == 200) and (response.json()['active']==True): #and the challenge is active
             self.challengeExists = True
             self.title = response.json()['title']
             self.instruction = response.json()['instruction']
-            print 'Challenge slug {slug}'.format(slug=self.slug),'exists.\n'
-        elif response.status_code == 500
+            print 'An active challenge slug, {slug},'.format(slug=self.slug),'exists.\n'
+        elif response.status_code == 500:
             print 'Could not assertain if challenge slug {slug} exists. Internal Server Error (500).\n'.format(slug=self.slug)
         else:
             print 'Challenge slug {slug}'.format(slug=self.slug),' does not exist. Creating challenge.\n'
-            createChallengeRequest = requests.put(self.api_createchallenge_endpoint.format(slug=slug),data=json.dumps({"title": self.title, "active": True}))
+            createChallengeRequest = requests.put(self.api_createchallenge_endpoint.format(slug=self.slug),data=json.dumps({"title": self.title, "active": True}))
             if createChallengeRequest.status_code == 200:
-                print "Challenge slug {slug} successfully created.\n".format(slug=self.slug)
+                print 'Challenge slug {slug} successfully created.\n'.format(slug=self.slug)
+                self.challengeExists = True
             else:
-                print "Could not create challenge slug {slug}.\n".format(slug=self.slug)
-                return False
+                print 'Could not create challenge slug {slug}.\n'.format(slug=self.slug)
+                return [False,'Could not create challenge slug {slug}.\n'.format(slug=self.slug)]
 
+    def addTask(self,inputMRTask):
+        #adds task dict to dict array of tasks (bulk payload). 
+        self.payload.append(inputMRTask.createPayload())
 
-    def createPayloadFromJSON(self,fileJSON):
-        with open(fileJSON, 'r') as infile:
-            tasks = json.load(infile)
-
-        for task in tasks:
-            osmid = task['geometries']['features'][0]['properties']['osmid']
-            geom = task['geometries']
-
-            mrtask = classmaprouletteloader.mapRouletteTask(geom,osmid,self.slug,self.instruction)
-            self.payload.append(mrtask.createPayload())
-
-
-    def createPayloadFromDatabase(self,dbUser,dbQuery,dbHost,dbName=None):
-        db_user = dbUser
-
-        db_name = dbName
-        if not dbName:
-            db_name = 'osm'
-
-        db_query = dbQuery
-
-        db_string = "dbname={db_name} user={db_user}".format(db_name=db_name,
-                                                             db_user=db_user
-                                                             )
-
-        if dbHost:
-            db_string += " host={db_host}".format(db_host=dbHost)
-
-        # open a connection, get a cursor
-        conn = psycopg2.connect(db_string)
-        cur = conn.cursor(cursor_factory=DictCursor)
-        register_hstore(cur)
-        # get our results
-        cur.execute(db_query)
-        nodes = cur.fetchall()
-
-        for node in nodes:
-            osmid = node["id"]
-
-            geom = {
-                "type": "FeatureCollection",
-                "features": [{
-                    "type": "Feature",
-                    "properties": {"osmid": osmid},
-                    "geometry": json.loads(geojson.dumps(
-                        wkb.loads(node["geom"].decode("hex"))))
-                }]
-            }        
-
-            mrtask = classmaprouletteloader.mapRouletteTask(geom,osmid,self.slug,self.instruction)
-            self.payload.append(mrtask.createPayload())            
-
+    def uploadTasks(self):
+        #converts dict payload to JSON. Then uploads tasks to MapRoulette Server.
+        headers = {'content-type': 'application/json'}
+        requestData = json.dumps(self.payload)
+        taskUploadRequest = requests.post(self.api_addbulktask_endpoint,data=requestData,headers=headers)  
+        return taskUploadRequest
 
 class mapRouletteTask(object):
     def __init__(self,geom,osmid,challengeSlug,challengeInstruction=None):
         self.identifier = None
-        self.geometries = geom
+        self.geom = geom
         self.slug = challengeSlug
         self.osmid = osmid
         self.payload = None
 
-        if instruction == None:
+        if challengeInstruction == None:
             self.instruction = ''
         else:
             self.instruction = challengeInstruction
@@ -116,11 +79,10 @@ class mapRouletteTask(object):
         #creates initial payload dict
         self.payload = {'instruction':self.instruction,'geom':self.geom}
 
-        #generates unique identifier and updates payload (dict)
+        #generates unique identifier and updates task payload (dict)
         digest = hashlib.md5(json.dumps(self.payload)).hexdigest()
         self.identifier = "{slug}-{osmid}-{digest}".format(slug=self.slug,osmid=self.osmid,digest=digest)
         self.payload.update({'identifier':self.identifier})
         
         #returns payload as dict 
-        return payload
-
+        return self.payload
