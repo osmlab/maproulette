07067b0dbe6fb34c031e238248b4d4ddace51d28
Martijn van Exel <m@rtijn.org>
2014-05-05 12:46:14 -0600

    proper geometry column creation on migrate

--- migrations/versions/3115f24a7604_.py
+++ migrations/versions/3115f24a7604_.py
@@ -11,16 +11,16 @@ revision = '3115f24a7604'
 down_revision = '38fe795129a0'
 
 from alembic import op
-import sqlalchemy as sa
-from geoalchemy2.types import Geometry
 
 
 def upgrade():
-    op.add_column('tasks', sa.Column('location', Geometry))
+    # there is no way to tell alembic to add a geometry column the right way?
+    op.execute(
+        "SELECT AddGeometryColumn('tasks', 'location', 4326, 'POINT', 2)")
     # there is no way to tell alembic that we want a GiST type index?
-    op.execute('CREATE INDEX idx_tasks_location ON tasks USING GIST(location)')
+    op.execute("CREATE INDEX idx_tasks_location ON tasks USING GIST(location)")
 
 
 def downgrade():
+    op.drop_index('idx_tasks_location', 'tasks')
     op.drop_column('tasks', 'location')
-    op.drop_index('idx_tasks_location')
