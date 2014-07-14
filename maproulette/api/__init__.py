f228af372f327a3c5dc2257f8a74eaba077ac660
Martijn van Exel <m@rtijn.org>
2014-07-14 16:08:06 -0600

    Adding metrics URL to config

--- maproulette/api/__init__.py
+++ maproulette/api/__init__.py
@@ -76,9 +76,7 @@ user_summary = {
     'display_name': fields.String
 }
 
-api = Api(app, decorators=[cors.crossdomain(origin=[
-    'maproulette-metrics-stage.s3-website-us-east-1.amazonaws.com',
-    'maproulette-metrics-production.s3-website-us-east-1.amazonaws.com'])])
+api = Api(app, decorators=[cors.crossdomain(origin=app.config['METRICS_URL'])])
 
 # override the default JSON representation to support the geo objects
 
