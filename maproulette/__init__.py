8077fc0594e681a46e653537e41994dc04fd4d94
Martijn van Exel <m@rtijn.org>
2014-07-14 17:40:04 -0600

    hot fix importing flask-KVSession

--- maproulette/__init__.py
+++ maproulette/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from simplekv.fs import FilesystemStore
-from flask_kvsession import KVSessionExtension
+from flask.ext.kvsession import KVSessionExtension
 
 # initialize server KV session store
 if not os.path.exists('./sessiondata'):
