0d8adec72eb0d51295bab4a563f98191d1928a50
Martijn van Exel <m@rtijn.org>
2014-07-22 18:11:32 -0600

    removing stale endpoints, cleaning up views

--- maproulette/views/__init__.py
+++ maproulette/views/__init__.py
@@ -4,12 +4,11 @@ from flask import render_template, redirect, session
 from maproulette import app
 from maproulette.helpers import signed_in
 
-from maproulette.models import User, Challenge, Task, TaskGeometry, Action, db
-from flask import Flask
+from maproulette.models import Challenge, Task, db
 from flask.ext.admin import Admin
 from flask.ext.admin.contrib.sqla import ModelView
-from flask.ext.sqlalchemy import SQLAlchemy
 from wtforms.fields import SelectField, TextAreaField
+
 
 class ChallengeAdminView(ModelView):
     column_list = ('slug', 'title', 'blurb')
@@ -19,14 +18,14 @@ class ChallengeAdminView(ModelView):
                           help=TextAreaField)
     form_args = dict(
         difficulty=dict(
-            choices=[ (1, 'Beginner'),
-                      (2, 'Intermediate'),
-                      (3, 'Advanced')],
+            choices=[(1, 'Beginner'),
+                     (2, 'Intermediate'),
+                     (3, 'Advanced')],
             coerce=int),
-        )
+    )
     form_widget_args = dict(
-        help = dict(rows="20", cols="200")
-        )
+        help=dict(rows="20", cols="200")
+    )
 
     def create_model(self, form):
         # We need to do this because of the way we instantiate challenges
@@ -36,12 +35,12 @@ class ChallengeAdminView(ModelView):
             self.session.add(model)
             self._on_model_change(form, model, True)
             self.session.commit()
-        except Exception as ex:
+        except Exception:
             if self._debug:
                 raise
 
-            flash(gettext('Failed to create model. %(error)s', error=str(ex)), 'error')
-            log.exception('Failed to create model')
+            # flash(gettext('Failed to create model. %(error)s', error=str(ex)), 'error')
+            app.logger.debug('Failed to create model')
             self.session.rollback()
             return False
         else:
@@ -50,9 +49,10 @@ class ChallengeAdminView(ModelView):
         return True
 
         # We need to override this because of the way we instantiate challenges
-        c = Challenge(form.slug, form.title)
+        # c = Challenge(form.slug, form.title)
     def __init__(self, session, **kwargs):
         super(ChallengeAdminView, self).__init__(Challenge, session, **kwargs)
+
 
 class TaskAdminView(ModelView):
     def __init__(self, session, **kwargs):
@@ -61,6 +61,7 @@ class TaskAdminView(ModelView):
 admin = Admin(app, name="MapRoulette")
 admin.add_view(ChallengeAdminView(db.session))
 admin.add_view(TaskAdminView(db.session))
+
 
 @app.route('/')
 def index():
@@ -83,16 +84,3 @@ def me():
     """Display a page about me with some stats
     and user settings."""
     return render_template('me.html')
-
-
-@app.route('/challenge/<challenge_slug>')
-def challenge_page(challenge_slug):
-    """Display a page about me with some stats
-    and user settings."""
-    return render_template('challenge.html', challenge_slug=challenge_slug)
-
-
-@app.route('/challenge_stats')
-def challenge_stats():
-    """Display the summary stats for all challenge_stats"""
-    return render_template('challenges.html')
