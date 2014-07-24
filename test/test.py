df768d2417e40fc213e6ed7c70a2533e03a16b25
Martijn van Exel <m@rtijn.org>
2014-07-23 19:29:25 -0600

    make test work, sort of

--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
 import os
-import maproulette
+from maproulette import app
+from maproulette.models import db
 import unittest
 import tempfile
 
@@ -7,14 +8,20 @@ import tempfile
 class MapRouletteTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, maproulette.app.config['DATABASE'] = tempfile.mkstemp()
-        maproulette.app.config['TESTING'] = True
-        self.app = maproulette.app.test_client()
-        maproulette.init_db()
+        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+        db.create_all()
 
     def tearDown(self):
+        db.drop_all()
         os.close(self.db_fd)
-        os.unlink(maproulette.app.config['DATABASE'])
+        os.unlink(app.config['DATABASE'])
+
+    def test_empty_db(self):
+        r = self.app.get('/api/challenges')
+        assert r.data.startswith('[]')
+
 
 if __name__ == '__main__':
     unittest.main()
