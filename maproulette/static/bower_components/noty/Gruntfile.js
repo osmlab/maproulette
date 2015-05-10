f7ce550dc732eb803107d7989b81f260b80c9c1e
Martijn van Exel <m@rtijn.org>
2015-05-10 12:09:55 +0200

    ignore more

--- maproulette/static/bower_components/noty/Gruntfile.js
+++ maproulette/static/bower_components/noty/Gruntfile.js
@@ -1,23 +1,47 @@
-module.exports = function (grunt) {
-	grunt.initConfig({
+module.exports = function(grunt) {
+    grunt.initConfig({
 
-		concat: {
-			dist: {
-				src: ['js/noty/jquery.noty.js', 'js/noty/layouts/*.js', 'js/noty/themes/*.js'],
-				dest: 'js/noty/packaged/jquery.noty.packaged.js'
-			}
-		},
+        bump  : {
+            options: {
+                files             : ['package.json', 'noty.jquery.json', 'bower.json', 'js/noty/jquery.noty.js'],
+                updateConfigs     : [],
+                commit            : false,
+                commitMessage     : 'Release v%VERSION%',
+                commitFiles       : ['-a'],
+                createTag         : true,
+                tagName           : 'v%VERSION%',
+                tagMessage        : 'Version %VERSION%',
+                push              : false,
+                pushTo            : 'upstream',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+            }
+        },
+        concat: {
+            dist: {
+                src : ['js/noty/jquery.noty.js', 'js/noty/layouts/*.js', 'js/noty/themes/*.js'],
+                dest: 'js/noty/packaged/jquery.noty.packaged.js'
+            }
+        },
 
-		uglify: {
-			minifyJS: {
-				files: {
-					'js/noty/packaged/jquery.noty.packaged.min.js': ['js/noty/jquery.noty.js', 'js/noty/layouts/*.js', 'js/noty/themes/*.js']
-				}
-			}
-		}
-	});
+        uglify: {
+            options : {
+                preserveComments: function(a) {
+                    return !!(a.start.file == 'js/noty/jquery.noty.js' && a.start.line == 11);
+                }
+            },
+            minifyJS: {
+                files: {
+                    'js/noty/packaged/jquery.noty.packaged.min.js': ['js/noty/jquery.noty.js', 'js/noty/layouts/*.js', 'js/noty/themes/*.js']
+                }
+            }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.registerTask('build', ['concat', 'uglify:minifyJS']);
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-bump');
+
+    grunt.registerTask('build', ['bump', 'concat', 'uglify:minifyJS']);
+    grunt.registerTask('conc', ['concat']);
+    grunt.registerTask('ugly', ['uglify:minifyJS']);
 };
