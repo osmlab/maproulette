4887d86d7b18850cc16ca37a7f4fe481aa29232d
Martijn van Exel <m@rtijn.org>
2015-08-03 16:42:16 -0600

    fixing Mapquest nominatim call

--- jsx/maproulette.js
+++ jsx/maproulette.js
@@ -714,9 +714,10 @@ var MRManager = (function () {
         };
 
         var displayAdminArea = function () {
-            var mqurl = 'http://open.mapquestapi.com/nominatim/v1/reverse?format=json&lat=' + map.getCenter().lat + ' &lon=' + map.getCenter().lng;
+            var mqurl = 'http://open.mapquestapi.com/nominatim/v1/reverse.php?format=json&lat=' + map.getCenter().lat + '&lon=' + map.getCenter().lng;
             $.ajax({
                 url: mqurl,
+                jsonp: "json_callback",
                 success: function (data) {
                     notify.play(MRHelpers.mqResultToString(data.address), {
                         timeout: false
