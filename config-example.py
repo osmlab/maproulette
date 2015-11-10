70a49de197e10f197f35a2a4d0a0fcc9a73b3d8c
Martijn van Exel <m@rtijn.org>
2015-11-10 15:29:56 -0700

    update config template

--- config-example.py
+++ config-example.py
@@ -7,17 +7,17 @@ SECRET_KEY = 'CHANGE THIS'
 
 # The OAuth configuration paramters for OSM.
 OSM = {
-    'base_url': 'http://www.openstreetmap.org/api/0.6/',
+    'base_url': 'http://master.apis.dev.openstreetmap.org/api/0.6/',
     'request_token_url':
-    'https://www.openstreetmap.org/oauth/request_token',
-    'access_token_url': 'https://www.openstreetmap.org/oauth/access_token',
-    'authorize_url': 'https://www.openstreetmap.org/oauth/authorize',
+    'http://master.apis.dev.openstreetmap.org/oauth/request_token',
+    'access_token_url': 'http://master.apis.dev.openstreetmap.org/oauth/access_token',
+    'authorize_url': 'http://master.apis.dev.openstreetmap.org/oauth/authorize',
     'consumer_key': 'CHANGE THIS',
-    'consumer_secret': 'CHANGE THIS'
+    'consumer_secret': 'CHANGE_THIS'
 }
 
 # Set debugging mode. This is detected by looking at the 'runserver' argument passed to manage.py
-DEBUG = (sys.argv[1] == 'runserver')
+DEBUG = (len(sys.argv)>1 and sys.argv[1] == 'runserver')
 
 # This is the buffer for looking for tasks / challenges near the given
 # lon/lat
@@ -35,7 +35,7 @@ LOGFILE = os.path.join(os.path.expanduser('~'), '/tmp/maproulette.log')
 LOGLEVEL = logging.DEBUG if DEBUG else logging.INFO
 
 # the default challenge to run
-DEFAULT_CHALLENGE = 'CHANGE THIS'
+DEFAULT_CHALLENGE = 'CHANGE_THIS'
 
 # IP Whitelist for external API calls
 # (/api/admin/*, /api/stats*, /api/users, /api/challenges)
@@ -53,3 +53,6 @@ MAX_TASKS_BULK_UPDATE = 5000
 # Basic Authentication user / pass
 AUTHORIZED_USER = 'testuser'
 AUTHORIZED_PASSWORD = 'password'
+
+# SQLAlchemy defaults
+SQLALCHEMY_TRACK_MODIFICATIONS = False
